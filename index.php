1979709d7302967d3603738380924b734c196069
stephbak <stephane.bakum@sfr.fr>
2018-12-14 16:07:12 +0100

    exercice8

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$addition = 3 + 4;
+$multiplication = 5 * 20;
+$division = 45 / 5;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>php partie1</title>
+</head>
+<body>
+  <p>3 + 4= <?= $addition; ?></p>
+  <p>5 * 20= <?= $multiplication; ?></p>
+  <p>45 / 5= <?= $division; ?></p>
+</body>
+</html>
